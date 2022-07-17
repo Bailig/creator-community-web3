ea843ac2ed4b315fdd03dd8525f5ab7cf54b1b74
Bailig <barrybailig@hotmail.com>
2022-07-17 21:15:40 +0300

    chore(contract): added deploy testnet

--- apps/smart-contract/hardhat.config.ts
+++ apps/smart-contract/hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+import { HardhatUserConfig } from "hardhat/config";
+
+const { GOERLI_URL, ACCOUNT_PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
--- apps/smart-contract/scripts/deploy.ts
+++ apps/smart-contract/scripts/deploy.ts
@@ -1,22 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  await lock.deployed();
+  const CreatorCommunity = await ethers.getContractFactory("CreatorCommunity");
+  const creatorCommunity = await CreatorCommunity.deploy();
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  await creatorCommunity.deployed();
+
+  console.log("Creator Community Address:", creatorCommunity.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
