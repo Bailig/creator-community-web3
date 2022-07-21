d02dfd5a82a20e73bdcbfcfd3431b6e60d11315a
Bailig <barrybailig@hotmail.com>
2022-07-21 18:01:44 +0300

    feat: routing

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { App } from "./containers";
 import "./index.css";
+
+const Home = () => <></>;
+const Profile = () => <></>;
+const UserPostList = () => <></>;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="/profile/:username" element={<Profile />} />
+          <Route
+            path="/user/:username/post/:postId"
+            element={<UserPostList />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
