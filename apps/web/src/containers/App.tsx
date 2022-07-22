c185adfd864d738c63c9ef640e6067a4df820981
Bailig <barrybailig@hotmail.com>
2022-07-22 09:59:58 +0300

    feat: code splitting

--- apps/web/src/containers/App.tsx
+++ apps/web/src/containers/App.tsx
@@ -1,3 +1,21 @@
-export const App = () => {
-  return <></>;
+import CreatorCommunity from "@cc/smart-contract/artifacts/contracts/CreatorCommunity.sol/CreatorCommunity.json";
+import { ethers } from "ethers";
+import { Navigation } from "./Navigation";
+
+const provider = new ethers.providers.JsonRpcProvider(
+  import.meta.env.VITE_GOERLI_URL
+);
+
+const creatorCommunityContract = new ethers.Contract(
+  import.meta.env.VITE_GOERLI_CONTRACT_ADDRESS,
+  CreatorCommunity.abi,
+  provider
+);
+
+export default () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
 };
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { App } from "./containers";
 import "./index.css";
+
+const App = React.lazy(() => import("./containers/App"));
 
 const Home = () => <></>;
 const Profile = () => <></>;
