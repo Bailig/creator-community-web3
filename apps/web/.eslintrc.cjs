44879b1a2f8075bbf32da2e656aaf6d120b7a94d
Bailig <barrybailig@hotmail.com>
2022-07-22 16:51:54 +0300

    chore: eslint config

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
+const eslintConfig = require("@cc/eslint-config");
+
+module.exports = eslintConfig;
