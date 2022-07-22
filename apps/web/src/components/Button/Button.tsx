98c94765d84eaf9187adda7f7a6ed16f128821c6
Bailig <barrybailig@hotmail.com>
2022-07-22 09:59:30 +0300

    feat: CreatePostDialog

--- apps/web/src/components/Button/Button.tsx
+++ apps/web/src/components/Button/Button.tsx
@@ -1,15 +1,16 @@
 import type { FC, ReactNode } from "react";
 
 export interface ButtonProps {
-  children: ReactNode;
+  className?: string;
+  children?: ReactNode;
   onClick?: () => void;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-  const { children, onClick } = props;
+  const { className = "", children, onClick } = props;
   return (
     <button
-      className="bg-gray-200 rounded-full px-4 h-10 cursor-pointer"
+      className={`bg-gray-200 rounded-full px-4 h-10 cursor-pointer ${className}`}
       onClick={onClick}
     >
       {children}
--- apps/web/src/components/IconButton/IconButton.tsx
+++ apps/web/src/components/IconButton/IconButton.tsx
@@ -1,14 +1,18 @@
 import type { FC, ReactNode } from "react";
 
 export interface IconButtonProps {
-  children: ReactNode;
+  className?: string;
+  children?: ReactNode;
   onClick?: () => void;
 }
 
 export const IconButton: FC<IconButtonProps> = (props) => {
-  const { children } = props;
+  const { className = "", children, onClick } = props;
   return (
-    <button className="rounded-full w-10 h-10 p-2 bg-gray-200 flex justify-center items-center text-xl">
+    <button
+      className={`rounded-full w-10 h-10 p-2 bg-gray-200 flex justify-center items-center text-xl ${className}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
